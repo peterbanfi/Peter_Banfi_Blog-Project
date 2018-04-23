3c0aafac6969d9bb13fa11d2761909a94ce5ecf7
Banfi Peter <p6t6rbanfi@gmail.com>
2018-04-23 09:43:24 +0200

    Feat: about button

--- FrontEnd/blog/src/app/home/home.component.ts
+++ FrontEnd/blog/src/app/home/home.component.ts
@@ -11,4 +11,8 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
   }
+  aboutThis() {
+    setInterval(() => document.querySelector('#my-about').innerHTML += (Math.floor(Math.random() * 2)), 100);
+
+  }
 }
